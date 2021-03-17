84754651020555b4d6364518ce13ecdbbb4569a8
Bernardo Albuquerque <marambaia.bma@gmail.com>
2021-03-17 19:12:54 -0300

    Main Scraping

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+res = requests.get('https://aws.amazon.com/pt/blogs/aws/')
+res.encoding = 'utf-8'
+soup = BeautifulSoup(res.text, 'html.parser')
+posts = soup.find_all(class_='blog-post')
+all_posts = []
+
+for post in posts:
+    title = post.find(class_='blog-post-title').text
+    preview = post.find(class_='blog-post-excerpt').text
+    image = post.find(class_='wp-post-image')['src']
+    meta = post.find(class_='blog-post-meta')
+    time = meta.time['datetime']
+    author = meta.find(property='author').text
+
+    all_posts.append({
+        'title': title,
+        'author': author,
+        'time': time,
+        'image': image,
+        'preview': preview
+    })
+
+with open('posts.json', 'w') as json_file:
+    json.dump(all_posts, json_file, indent=3, ensure_ascii=False)
